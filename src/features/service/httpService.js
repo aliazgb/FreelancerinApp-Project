141f3a96087ee159206e0a216698a4b26b62155d
aliazgb <ali.azgb10@gmail.com>
2025-02-27 17:47:06 +0100

    solution for refresh token request

--- src/features/service/httpService.js
+++ src/features/service/httpService.js
@@ -1,18 +1,25 @@
 import axios from "axios";
-
+const BASE_URL = "http://localhost:5000/api";
 const app = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 app.interceptors.request.use(
-  (res) =>res,
-  async (err)=>{
-    const originalConfig=err.config;
-    if (err.response.status==401) {
-      
+  (res) => res,
+  async (err) => {
+    const originalConfig = err.config;
+    if (err.response.status == 401) {
+      try {
+        const { date } = await axios.get(`${BASE_URL}/user/refresh-token`, {
+          withCredentials: true,
+        });
+        if (date) {
+          return app(originalConfig);
+        }
+      } catch (error) {}
     }
   }
-)
+);
 const http = {
   get: app.get,
   post: app.post,
