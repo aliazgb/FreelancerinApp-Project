e2ab2325e939a9e02e6ffbf2807e5acee8850148
YourGitHubUsername <YourEmail@example.com>
2025-03-23 21:19:16 +0100

    try for adding on vercel

--- src/features/freelancer/FreelancerLayout.jsx
+++ src/features/freelancer/FreelancerLayout.jsx
@@ -2,29 +2,29 @@ import { HiCollection } from "react-icons/hi";
 import { HiHome } from "react-icons/hi2";
 import AppLayout from "../ui/AppLayout";
 import Sidebar from "../ui/Sidebar";
-import CustomNavLink from "../ui/CustomNavLink";
+import CousttomLinks from "../ui/CousttomLinks";
 
 function FreelancerLayout() {
   return (
     <AppLayout>
       <Sidebar>
         <li>
-          <CustomNavLink to="dashboard"> 
+          <CousttomLinks to="dashboard"> 
             <HiHome />
             <span>داشبورد</span>
-          </CustomNavLink>
+          </CousttomLinks>
         </li>
         <li>
-          <CustomNavLink to="projects">
+          <CousttomLinks to="projects">
             <HiCollection />
             <span>پروژه ها</span>
-          </CustomNavLink>
+          </CousttomLinks>
         </li>
         <li>
-          <CustomNavLink to="proposals">
+          <CousttomLinks to="proposals">
             <HiCollection />
             <span>درخواست ها</span>
-          </CustomNavLink>
+          </CousttomLinks>
         </li>
       </Sidebar>
     </AppLayout>
--- src/features/ui/CousttomLinks.jsx
+++ src/features/ui/CousttomLinks.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 
-function CustomNavLink({ children, to }) {
+function CousttomLinks({ children, to }) {
   const navLinkClasses =
     "flex items-center gap-x-2 hover:bg-primary-100 px-2 py-2 rounded-lg w-[80%]";
   return (
@@ -16,4 +16,4 @@ function CustomNavLink({ children, to }) {
     </NavLink>
   );
 }
-export default CustomNavLink ;
+export default CousttomLinks ;
